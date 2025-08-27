7389c7adbc1fcf6d888132d9f0a2b30f96c66657
Olegeros <omegapro02aj@gmail.com>
2025-08-27 16:15:31 +0300

    feat: pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+
+  str.split('').forEach((char) => {
+    bracketsConfig.forEach(([open, close]) => {
+      if (char === open && char === close) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else if (char === open) {
+        stack.push(char);
+      } else if (char === close) {
+        if (stack.pop() !== open) {
+          stack.push(null);
+        }
+      }
+    });
+  });
+
+  return stack.length === 0;
 };
